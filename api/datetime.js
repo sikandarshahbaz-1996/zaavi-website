b39a5a119ffc47b5edd653c4836b0729922d67bd
Sikandar Shahbaz <138167497+sikandarshahbaz-1996@users.noreply.github.com>
2025-04-02 22:02:21 -0400

    Update datetime.js

--- api/datetime.js
+++ api/datetime.js
@@ -1,15 +1,20 @@
 export default function handler(req, res) {
-  const today = new Date();
+  const date = new Date();
 
-  const utcDateTime = today.toISOString(); // UTC datetime
-  const torontoDayOfWeek = today.toLocaleString('en-CA', {
-    weekday: 'long',
+  const options = {
     timeZone: 'America/Toronto',
-  });
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  };
+
+  const formattedDate = date.toLocaleString('en-CA', options).replace(',', '');
 
   res.status(200).json({
-    datetime: utcDateTime,
-    dayOfWeek: torontoDayOfWeek,
-    timezone: 'UTC'
+    datetime: formattedDate,
+    dayOfWeek: date.toLocaleString('en-US', { weekday: 'long', timeZone: 'America/Toronto' }),
   });
 }
